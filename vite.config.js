b34b55051279fa0588e2b61960a0c613a5c00ce8
yahya <yahya23ti@mahasiswa.pcr.ac.id>
2025-05-20 11:19:35 +0700

    sfasdf

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    react(),
+    tailwindcss(),
+  ]
   
 })
